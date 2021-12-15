5100a2f2fa56cd9d22d130379e87aeb03a5345ca
py-ma <thoughts.love@mail.ru>
2021-12-15 20:25:10 +0300

    update

--- main.py
+++ main.py
@@ -1,23 +1,28 @@
+"""main"""
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.types import ReplyKeyboardRemove
 from aiogram.utils import executor
 from states import *
 import markups as m
-import time
 import config
 
 bot = Bot(token=config.BOT_TOKEN)
 dp = Dispatcher(bot)
 ORDER_DATA = {}
 
+
 # -----------------------------------------START--------------------------------------------------
 @dp.message_handler(commands=['start'], state=None)
 async def start(message: types.Message):
-    s.t1()
-    await bot.send_message(message.from_user.id, "Привет!\nКакую ты хочешь пиццу?")
-    ORDER_DATA[str(message.from_user.id)] = {}
-    return await choose_size(message)
+    """
+    start
+    """
+    if message.text == '/start':
+        s.t1()
+        await bot.send_message(message.from_user.id, "Привет!\nКакую ты хочешь пиццу?")
+        ORDER_DATA[str(message.from_user.id)] = {}
+        return await choose_size(message)
 
 
 # -----------------------------------------message_handler--------------------------------------------------
@@ -28,18 +33,20 @@ async def waiting_yes(message: types.Message):
         ORDER_DATA[str(message.from_user.id)]['size'] = message.text
         return await choose_pay_method(message)
 
-    elif message.text in ('Наличными', 'Картой'):
+    if message.text in ('Наличными', 'Картой'):
         ORDER_DATA[str(message.from_user.id)]['pay'] = message.text
         return await repeating_order(message)
 
-    elif message.text == 'Да':
+    if message.text == 'Да':
         s.t5()
         return await finish(message)
 
-    elif message.text == 'Нет':
+    if message.text == 'Нет':
         s.tn()
         await bot.send_message(message.from_user.id, 'Ну что же, давай по-новой')
         return await choose_size(message)
+    else:
+        return await bot.send_message(message.from_user.id, 'Не понимаю тебя\nВыбери кнопку из меню ')
 
 
 # -----------------------------------------SIZE---------------------------------------------------
@@ -52,21 +59,19 @@ async def choose_size(message: types.Message):
 # -----------------------------------------PAY----------------------------------------------------
 async def choose_pay_method(message: types.Message):
     s.t4()
-    return await bot.send_message(message.from_user.id, 'Выбери способ оплаты',
-                                  reply_markup=m.pay_menu)
+    return await bot.send_message(message.from_user.id, 'Выбери способ оплаты', reply_markup=m.pay_menu)
 
 
-# --------------------------REPEATING AND WAITING YES---------------------------------------------
+# --------------------------REPEATING AND WAITING YES---------------------
 async def repeating_order(message: types.Message):
     size = ORDER_DATA[str(message.from_user.id)]['size'].lower()
     pay = ORDER_DATA[str(message.from_user.id)]['pay'].lower()
     return await bot.send_message(message.from_user.id,
-                                      f'Вы хотите {size} пиццу, оплата - {pay}?',
-                                      reply_markup=m.finalMenu)
+                                  f'Вы хотите {size} пиццу, оплата - {pay}?',
+                                  reply_markup=m.finalMenu)
 
 
 # -------------------------------------FINISH-----------------------------------------------------
-@dp.message_handler()
 async def finish(message: types.Message):
     s.t6()
     final_menu = ReplyKeyboardRemove()
--- states.py
+++ states.py
@@ -1,5 +1,7 @@
 from transitions import Machine, State
 from main import *
+
+
 class Model(object):
     states = ['START',
               'CHOOSE SIZE',
@@ -8,21 +10,23 @@ class Model(object):
               'REPEATING ORDER',
               'FINISH']
 
+
 s = Model()
 transitions = [
     ['t1', 'START', 'CHOOSE SIZE'],
-    ['t2', 'CHOOSE SIZE', 'GO PAY'],
-    ['t3', 'GO PAY', 'CHOOSE PAY METHOD'],
-    ['t4', 'CHOOSE PAY METHOD', 'REPEATING ORDER'],
-    ['t5', 'REPEATING ORDER', 'FINISH'],
-    ['tn', 'REPEATING ORDER', 'CHOOSE SIZE'],
-    ['t6', 'FINISH', 'START']]
+    ['t2', 'CHOOSE SIZE', 'GO PAY' or 'START'],
+    ['t3', 'GO PAY', 'CHOOSE PAY METHOD' or 'START'],
+    ['t4', 'CHOOSE PAY METHOD', 'REPEATING ORDER' or 'START'],
+    ['t5', 'REPEATING ORDER', 'FINISH' or 'START'],
+    ['tn', 'REPEATING ORDER', 'START'],
+    ['t6', 'FINISH', 'START']
+]
 
-machine = Machine(model = s,states=Model.states, initial='START')
-machine.add_transition('t1', source='START', dest='CHOOSE SIZE')
-machine.add_transition('t2', source='CHOOSE SIZE', dest='GO PAY')
-machine.add_transition('t3', source='GO PAY', dest='CHOOSE PAY METHOD')
-machine.add_transition('t4', source='CHOOSE PAY METHOD', dest='REPEATING ORDER')
+machine = Machine(model=s, states=Model.states, initial='START')
+machine.add_transition('t1', source='START', dest='CHOOSE SIZE' or 'START')
+machine.add_transition('t2', source='CHOOSE SIZE', dest='GO PAY' or 'START')
+machine.add_transition('t3', source='GO PAY', dest='CHOOSE PAY METHOD' or 'START')
+machine.add_transition('t4', source='CHOOSE PAY METHOD', dest='REPEATING ORDER' or 'START')
 machine.add_transition('t5', source='REPEATING ORDER', dest='FINISH')
 machine.add_transition('tn', source='REPEATING ORDER', dest='CHOOSE SIZE')
 machine.add_transition('t6', source='FINISH', dest='START')
